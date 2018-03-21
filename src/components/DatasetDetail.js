34425dca4f9642588e14b0b750349937ce70be62
Felipe Pontes <felipemfpontes@gmail.com>
2018-03-21 11:57:03 -0300

    Refactor DatasetDetail into PaginatedTable

--- src/components/DatasetDetail.js
+++ src/components/DatasetDetail.js
@@ -2,22 +2,10 @@ import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 import { Section, Title, Subtitle, Container, Table, Tag, Pagination, Page, PageLink, PageControl, PageEllipsis, PageList } from 'bloomer'
 
-import {PER_PAGE} from '../constants'
+import PaginatedTable from './PaginatedTable'
 
 class DatasetDetail extends Component {
-  state = {
-    limit: PER_PAGE,
-    offset: 0
-  }
-
-  componentWillReceiveProps(nextProps, nextContext) {
-    if (nextContext.data === null) {
-      this.setState({limit: PER_PAGE, offset: 0})
-    }
-  }
-
   render() {
-    const {offset, limit} = this.state
     const {data} = this.context
 
     return (
@@ -26,41 +14,29 @@ class DatasetDetail extends Component {
         <Subtitle isSize={6}>
           Este é o resumo do conjunto de dados escolhido
         </Subtitle>
-        <div>
-          {data === null
-            ? 'Loading...'
-            : <Fragment>
-                <Table isStriped isNarrow style={{width: "100%"}}>
-                  <thead>
-                    <tr>
-                      <th>Matrícula</th>
-                      <th>MF</th>
-                      <th>I.R.A.</th>
-                      <th>Frequência</th>
-                      <th>TF</th>
-                      <th>Renda</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.slice(offset, limit).map((item, idx) => (
-                      <tr key={idx}>
-                        <td>{item.matricula}</td>
-                        <td>{item.mediaFinal}</td>
-                        <td>{item.coefRendimento}</td>
-                        <td>{item.frequencia}</td>
-                        <td>{item.faltas}</td>
-                        <td>R$ {item.renda.toFixed(2)}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-                <Pagination>
-                    <PageControl disabled={offset <= 0} onClick={() => offset > 0 && this.setState({offset: offset-PER_PAGE, limit: offset})}>Anterior</PageControl>
-                    <PageControl isNext onClick={() => this.setState({offset: limit, limit: limit+PER_PAGE})}>Próxima</PageControl>
-                </Pagination>
-              </Fragment>
-          }
-        </div>
+
+        <PaginatedTable 
+          data={data}
+          columns={[{
+            label: 'Matrícula',
+            value: 'matricula'
+          }, {
+            label: 'MF',
+            value: 'mediaFinal'
+          }, {
+            label: 'I.R.A.',
+            value: 'coefRendimento'
+          }, {
+            label: 'Frequência',
+            value: 'frequencia'
+          }, {
+            label: 'TF',
+            value: 'faltas'
+          }, {
+            label: 'Renda',
+            rendered: (item, key) => <td key={key}>R$ {item.renda.toFixed(2)}</td>
+          }]}
+        />
       </div>
     )
   }
---
+++ src/components/PaginatedTable.js
@@ -0,0 +1,59 @@
+import React, {Component, Fragment} from 'react'
+import PropTypes from 'prop-types'
+import { Section, Title, Subtitle, Container, Table, Tag, Pagination, Page, PageLink, PageControl, PageEllipsis, PageList } from 'bloomer'
+
+import {PER_PAGE} from '../constants'
+
+class PaginatedTable extends Component {
+  state = {
+    limit: PER_PAGE,
+    offset: 0
+  }
+
+  componentWillReceiveProps({data}) {
+    if (data === null) {
+      this.setState({limit: PER_PAGE, offset: 0})
+    }
+  }
+
+  render() {
+    const {offset, limit} = this.state
+    const {data, columns} = this.props
+
+    return (
+      <div>
+        {data === null
+          ? 'Loading...'
+          : <Fragment>
+              <Table isStriped isNarrow style={{width: '100%'}}>
+                <thead>
+                  <tr>
+                    {columns.map((c, key) => <th key={key}>{c.label}</th>)}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.slice(offset, limit).map((item, idx) => (
+                    <tr key={idx}>
+                      {columns.map((c, key) => {
+                        if (c.renderer) {
+                          return c.renderer(item, key)
+                        }
+
+                        return <td key={key}>{item[c.value]}</td>
+                      })}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <Pagination>
+                  <PageControl disabled={offset <= 0} onClick={() => offset > 0 && this.setState({offset: offset-PER_PAGE, limit: offset})}>Anterior</PageControl>
+                  <PageControl isNext onClick={() => this.setState({offset: limit, limit: limit+PER_PAGE})}>Próxima</PageControl>
+              </Pagination>
+            </Fragment>
+        }
+      </div>
+    )
+  }
+}
+
+export default PaginatedTable
